751981170ef86d94f03dd1f4c408035e9655923f
Deepak Kanshetti <kanshettid@gmail.com>
2023-12-17 22:47:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function firstChar(text) {
   // your code here
-	if(text.length == 0)
-		return '';
-	else if(text == ' ')
-		return '';
+	if(text.length == 0 || text == ' ')
+		return text[1];
+	// else if(text == ' ')
+	// 	return '';
 	else if(text.length > 0){
 		text = text.trim();
 		return text[0];
